80f49982b6e1146ef17b8c8af55441f32472b3ba
rudininh <sorows22@gmail.com>
2022-01-26 21:21:04 +0800

    tes push

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts/main')
 
 @section('container')
-    <h1>THIS IS ABOUT YOU AND ME</h1>
+    <h1>THIS IS ABOUT YOU AND ME asda</h1>
     <h1>{{ $name }} </h1>
     <h2>  {{ $email }} </h2>
     <img src="{{ $img }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
