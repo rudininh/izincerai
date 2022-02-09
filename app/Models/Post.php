b5f01e4527327ded78e9517ce3f00b8bfdcdd81e
rudininh <sorows22@gmail.com>
2022-02-10 07:38:16 +0800

    Part 8 Eloquent Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     // protected $fillable = ['title','excerpt','body'];
     protected $guarded = ['id'];
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
