0e2c0924b1318022b529ad0f043a13eb9cc59e6f
rudininh <sorows22@gmail.com>
2022-02-14 22:16:33 +0800

    Part 9 Started

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,15 +2,20 @@
 @extends('layouts/main')
 
     @section('container')
+    <h1>Halaman Blog Posts</h1>
+
 
     @foreach ($posts as $post)
-    <article class="mb-5">
+    <article class="mb-5 border-bottom pb-4">
         <h2>
-            <a href="posts/{{ $post->slug  }}" class="href">
+            <a href="posts/{{ $post->slug  }}" class="text-decoration-none">
                 {{ $post->title }}
             </a>
         </h2>
+             <p>By. Rudini in a  <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
         <p>{{ $post->excerpt  }}</p>
+
+        <a href="posts/{{ $post->slug  }}" class="text-decoration-none">Read More..</a>
     </article>
 
     
