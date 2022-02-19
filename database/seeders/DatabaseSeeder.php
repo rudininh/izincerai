212ffd8f760a4445f154ef7a7328f6b23fe500c9
rudininh <sorows22@gmail.com>
2022-02-20 00:02:42 +0800

    rtes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,19 +19,21 @@ class DatabaseSeeder extends Seeder
     {
       // \App\Models\User::factory(10)->create();
 
-       User::create([
-        'name' => 'Rudini',
-        'email' => 'rudini@gmail.com',
-        'password' =>bcrypt('12345')
+      //  User::create([
+      //   'name' => 'Rudini',
+      //   'email' => 'rudini@gmail.com',
+      //   'password' =>bcrypt('12345')
 
-      ]);
+      // ]);
 
-        User::create([
-        'name' => 'Agustina',
-        'email' => 'agustina@gmail.com',
-        'password' =>bcrypt('12345')
+      //   User::create([
+      //   'name' => 'Agustina',
+      //   'email' => 'agustina@gmail.com',
+      //   'password' =>bcrypt('12345')
 
-      ]);
+      // ]);
+
+      
 
        Category::create([
         'name' => 'Web Programming',
@@ -48,7 +50,7 @@ class DatabaseSeeder extends Seeder
       Post::create([
         'title' => 'Judul Pertama',
         'slug' => 'judul-pertama',
-        'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quasi ducimus magnam unde culpa architecto omnis necessitatibus? Perferendis harum ',
+        'excerpt' => 'Lorem ipsusssm dolor sit amet, consectetur adipisicing elit. Quisquam quasi ducimus magnam unde culpa architecto omnis necessitatibus? Perferendis harum ',
         'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam quasi ducimus magnam unde culpa architecto omnis necessitatibus? Perferendis harum impedit hic dolorum, aut reprehenderit, molestiae culpa numquam consequatur repellendus facilis quia vitae praesentium voluptates dicta ut deserunt consectetur porro distinctio temporibus veritatis. Repudiandae ratione, eligendi nobis eveniet inventore aperiam, possimus tenetur id eius earum aliquid voluptatem officiis odio est ipsa asperiores iusto vitae sint voluptates labore iure rerum. Rerum voluptatem maxime aperiam asperiores libero, soluta quam quis officiis. Veritatis reiciendis sunt perspiciatis corrupti. Voluptatibus, itaque in cumque consequatur totam natus labore libero iusto consectetur sequi laboriosam doloribus nam quaerat. Nulla.',
         'category_id' => 1,
         'user_id' => 1
