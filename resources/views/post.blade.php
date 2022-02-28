a9b85d55928949d9cf19f422439a8eca505cb613
rudininh <sorows22@gmail.com>
2022-02-28 23:22:09 +0800

    back

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,5 +4,5 @@
 <h2 class="mb-5">{{ $post->title }}</h2>
 <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in a  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 {!! $post->body !!}
-<a href="/posts" class="d-block mt-3">Back To Posts</a>
+<a href="/posts" class="d-block mt-3">Back To Post</a>
 @endsection
