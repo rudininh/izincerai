726c6bae79e439934f1e33548ef230360908d51b
rudininh <sorows22@gmail.com>
2022-03-01 23:28:45 +0800

    Part 12 Minor

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
      <div class="card mb-3">
         <img src="http://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
         <div class="card-body text-center">
-            <h3 class="card-title">{{ $posts[0]->title }}</h3>
+            <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}">{{ $posts[0]->title }}</a></h3>
             <small class="text-muted"><p>By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in a  <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}</p></small>
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
              <a href="/posts/{{ $posts[0]->slug  }}" class="text-decoration-none btn btn-primary">Read More</a>
