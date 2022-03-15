55bed015f0ad8fdd2e19fc0fa275d028d9ea2d11
rudininh <sorows22@gmail.com>
2022-03-15 23:08:50 +0800

    Part 12 Posts Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -29,6 +29,7 @@
             @foreach ($posts->skip(1) as $post)
             <div class="col-md-4 mb-3">
             <div class="card" style="width: 18rem;">
+            <div class="position-absolute px-3 py-2" style="background-color: rgba(0,0,0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none" >{{ $post->category->name }}</a></div>
             <img src="http://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }}</h5>
