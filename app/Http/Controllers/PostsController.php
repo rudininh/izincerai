b39c3be464ecdfdad67eba603e71802c4d0324ee
rudininh <sorows22@gmail.com>
2022-04-03 14:08:39 +0800

    Part 13 Query Scope

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -7,17 +7,11 @@ class PostsController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
-
-        if(request('search')){
-            $posts->where('title', 'like', '%' . request('search') . '%')
-                  ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
-
+      
         return view('posts', [
         "title" => "All Posts",
         "active" => "posts",
-        "posts" => $posts->get()
+        "posts" => Post::latest()->filter(request(['search']))->get()
 
         ]);
     }
