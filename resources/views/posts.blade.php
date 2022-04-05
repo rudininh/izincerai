00b41eafe28010fb2e04a7f6a0a801f938fe14ae
rudininh <sorows22@gmail.com>
2022-04-05 23:43:23 +0800

    Part 13 Category dan Search

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,6 +7,9 @@
     <div class="row justify-content-center mb-3">
     <div class="col-md-6">
         <form action="/posts">
+            @if (request('category'))
+                <input type="hidden" name="category" value="{{ request('category') }}">
+            @endif
             <div class="input-group mb-3">
                 <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
                 <button class="btn btn-danger" type="submit" id="">Search</button>
@@ -20,8 +23,8 @@
         <img src="http://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
         <div class="card-body text-center">
             <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h3>
-           <p>
-                <small class="text-muted">By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in a  <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+           <p>s
+                <small class="text-muted">By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in a  <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
                 </small>
             </p>
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -35,7 +38,7 @@
             @foreach ($posts->skip(1) as $post)
             <div class="col-md-4 mb-3">
             <div class="card" style="width: 18rem;">
-            <div class="position-absolute px-3 py-2" style="background-color: rgba(0,0,0, 0.7)"><a href="/categories/{{ $post->category->slug }}" class="text-white text-decoration-none" >{{ $post->category->name }}</a></div>
+            <div class="position-absolute px-3 py-2" style="background-color: rgba(0,0,0, 0.7)"><a href="/posts?category={{ $post->category->slug }}" class="text-white text-decoration-none" >{{ $post->category->name }}</a></div>
             <img src="http://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
             <div class="card-body">
                 <h5 class="card-title">{{ $post->title }}</h5>
