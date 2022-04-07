efc0e96264857047be875c9bcab48b00a8c92c59
rudininh <sorows22@gmail.com>
2022-04-07 23:48:11 +0800

    minor

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -50,7 +50,7 @@
                 </small>
             </p>
                 <p class="card-text">{{ $post->excerpt  }}</p>
-                <a href="/posts/{{ $post->slug  }}" class="btn btn-primary">Reads more</a>
+                <a href="/posts/{{ $post->slug  }}" class="btn btn-primary">Read more</a>
             </div>
             </div>
             </div> 
