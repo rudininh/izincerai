382170a0651dd9800e5b5395318740b5fb5cc6d4
rudininh <sorows22@gmail.com>
2022-04-19 15:45:18 +0800

    Part 15 Half

--- routes/web.php
+++ routes/web.php
@@ -60,3 +60,4 @@ Route::get('/login', [LoginController::class, 'index']);
 
 Route::get('/register', [RegisterController::class, 'index']);
 
+Route::post('/register', [RegisterController::class, 'store']);
