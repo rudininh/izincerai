15021402ae34f060507ecae70176cb6a489a77c4
rudininh <sorows22@gmail.com>
2022-05-08 00:40:18 +0800

    Part 17

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -14,7 +14,10 @@ class DashboardPostController extends Controller
      */
     public function index()
     {
-        return view('dashboard.posts.index');
+    
+        return view('dashboard.posts.index', [
+            'posts' => Post::where('user_id', auth()->user()->id)->get()
+        ]);
     }
 
     /**
