b94c5dbe3ee6c0db7e4c5d51075bae80eb4aaded
rudininh <sorows22@gmail.com>
2022-06-01 22:02:15 +0800

    Add pst

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -4,7 +4,7 @@
  
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Create New Post</h1>
+        <h1 class="h2">Create New Posts</h1>
     </div>
 <div class="col-lg-8">
  <form method="post" action="/dashboard/posts">
@@ -27,16 +27,16 @@
   </div>
 
 
-   <script>
-  const title = document.querySelector('#title');
-  const slug = document.querySelector('#slug');
+      <script>
+      const title = document.querySelector('#title');
+      const slug = document.querySelector('#slug');
 
-  title.addEventListener('change', function(){
-    fetch('/dashboard/post/checkSlug?title=' + title.value)
-      .then(response => response.json())
-      .then(data => slug.value = data.slug)
-  });
-</script> 
+      title.addEventListener('change', function(){
+        fetch('/dashboard/post/checkSlug?title=' + title.value)
+          .then(response => response.json())
+          .then(data => slug.value = data.slug)
+      });
+    </script> 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script>
