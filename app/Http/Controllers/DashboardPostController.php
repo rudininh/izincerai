4a5fe2039148f6460852ba2c6ce9a0c013f73585
rudininh <sorows22@gmail.com>
2022-06-15 06:40:19 +0800

    Part 18 Done

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -42,7 +42,13 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validatedData = $request->validate([
+
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body'=> 'required'
+        ]);
     }
 
     /**
