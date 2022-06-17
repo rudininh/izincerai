631d0aef1e833866d4dd6d8dfdfbc792a90f5d1a
rudininh <sorows22@gmail.com>
2022-06-18 06:47:49 +0800

    Menyimpan data ke tabel post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -49,6 +50,14 @@ class DashboardPostController extends Controller
             'category_id' => 'required',
             'body'=> 'required'
         ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['exceprt'] = Str::limit(strip_tags($request->body), 200);
+
+
+        Post::create($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', "New post hash been added!");
     }
 
     /**
