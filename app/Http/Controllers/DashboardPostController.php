d4dc0e367abcf05c18b5c948f12bab4ef2301ccb
rudininh <sorows22@gmail.com>
2022-06-23 16:11:20 +0800

    Part 20 Update & Delete Post Done

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -97,7 +97,30 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body'=> 'required'
+        ];
+
+        if($request->slug != $post->slug){
+            $rules['slug'] = 'required|unique:posts';
+        };
+    
+        $validatedData = $request->validate($rules);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+
+        Post::where('id', $post->id)
+            ->update($validatedData);
+
+        return redirect('/dashboard/posts')->with('success', "Post has been updated!");
+  
+       
+
     }
 
     /**
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -9,7 +9,7 @@
 
 
            <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my posts</a>
-           <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+           <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
            
            
 
