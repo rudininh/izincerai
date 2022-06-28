91e197275023efa756b1572a501a9be9f59019a3
rudininh <sorows22@gmail.com>
2022-06-28 15:21:27 +0800

    Part 21 Done menampilkan image pada post

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -19,9 +19,15 @@
                   <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span> Delete</button>
             </form>
 
+            @if($post->image)
 
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid mt-3" alt="{{ $post->category->name }}">
+            </div>
+
+            @else
             <img src="http://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid mt-3" alt="{{ $post->category->name }}">
-            
+            @endif
             <article class="my-3 fs-5">    
                 {!! $post->body !!}
             </article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,17 @@
 
             <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in a  <a href="/posts?category={{ $post->category->slug }}">{{ $post->category->name }}</a></p>
 
+            @if($post->image)
+
+            <div style="max-height: 350px; overflow:hidden;">
+            <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid " alt="{{ $post->category->name }}">
+            </div>
+
+            @else
             <img src="http://source.unsplash.com/1200x400?{{ $post->category->name }}" class="img-fluid" alt="{{ $post->category->name }}">
+            @endif
+
+            
             
             <article class="my-3 fs-5">    
                 {!! $post->body !!}
