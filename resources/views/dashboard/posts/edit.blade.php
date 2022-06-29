3291056b853e708fa9f97ca21fd91bbda130f766
rudininh <sorows22@gmail.com>
2022-06-29 21:19:44 +0800

    Edit Post

--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -4,7 +4,7 @@
  
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Edit Post</h1>
+        <h1 class="h2">Edit Posts</h1>
     </div>
 <div class="col-lg-8">
  <form method="post" action="/dashboard/posts/{{ $post->slug }}" class="mb-5" enctype="multipart/form-data">
