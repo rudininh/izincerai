e9b8b6bbb5f9d77b063360bda3dabab31fa8cce1
rudininh <sorows22@gmail.com>
2022-07-19 06:24:19 +0800

    Membuat folder dan file dashboard categories

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -14,7 +14,9 @@ class AdminCategoryController extends Controller
      */
     public function index()
     {
-        return "ini adalah halaman categories";
+        return view('dashboard.categories.index', [
+            'categories' => Category::all()
+        ]);
     }
 
     /**
