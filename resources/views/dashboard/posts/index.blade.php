7c91f2d05f31f98c705063e7a69a06250b3b71b8
sorows22@gmail.com <sorows22@gmail.com>
2022-08-20 00:08:22 +0800

    good l

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -18,7 +18,7 @@
           <thead>
             <tr>
               <th scope="col">No</th>
-              <th scope="col">Title</th>
+              <th scope="col">Nama</th>
               <th scope="col">Category</th>
               <th scope="col">Action</th>
             </tr>
