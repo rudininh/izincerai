6f4b6fc3b931ce269d072c0ade80c758fe52975d
sorows22@gmail.com <sorows22@gmail.com>
2022-08-20 23:25:27 +0800

    jabatan

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -20,7 +20,7 @@
               <th scope="col">No</th>
               <th scope="col">Nama</th>
               <th scope="col">NIP</th>
-              <th scope="col">Action</th>
+              <th scope="col">JAbatan</th>
             </tr>
           </thead>
           <tbody>
