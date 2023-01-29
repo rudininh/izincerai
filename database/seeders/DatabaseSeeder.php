f67707a7089c08040a052f3373d855fe7abca4dd
rudininh <sorows22@gmail.com>
2023-01-30 06:46:07 +0800

    Menambahkan isi field kolom pangkat

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -57,6 +57,90 @@ class DatabaseSeeder extends Seeder
         'golongan_pangkat' => 'I/',
         'ruang_pangkat' => 'a'
       ]);
+      
+      Pangkat::create([
+        'nama_pangkat' => 'Juru Muda Tk. I',
+        'golongan_pangkat' => 'I/',
+        'ruang_pangkat' => 'b'
+      ]);
+      Pangkat::create([
+        'nama_pangkat' => 'Juru',
+        'golongan_pangkat' => 'I/',
+        'ruang_pangkat' => 'c'
+      ]);
+      Pangkat::create([
+        'nama_pangkat' => 'Juru Tk. I',
+        'golongan_pangkat' => 'I/',
+        'ruang_pangkat' => 'd'
+      ]);
+      Pangkat::create([
+        'nama_pangkat' => 'Pengatur Muda',
+        'golongan_pangkat' => 'II/',
+        'ruang_pangkat' => 'a'
+      ]);
+          Pangkat::create([
+        'nama_pangkat' => 'Pengatur Muda Tk. I',
+        'golongan_pangkat' => 'II/',
+        'ruang_pangkat' => 'b'
+      ]);
+          Pangkat::create([
+        'nama_pangkat' => 'Pengatur',
+        'golongan_pangkat' => 'II/',
+        'ruang_pangkat' => 'c'
+      ]);
+          Pangkat::create([
+        'nama_pangkat' => 'Pengatur Tk. I',
+        'golongan_pangkat' => 'II/',
+        'ruang_pangkat' => 'd'
+      ]);
+          Pangkat::create([
+        'nama_pangkat' => 'Penata Muda',
+        'golongan_pangkat' => 'III/',
+        'ruang_pangkat' => 'a'
+      ]);
+             Pangkat::create([
+        'nama_pangkat' => 'Penata Muda Tk. I',
+        'golongan_pangkat' => 'III/',
+        'ruang_pangkat' => 'b'
+      ]);
+        Pangkat::create([
+        'nama_pangkat' => 'Penata',
+        'golongan_pangkat' => 'III/',
+        'ruang_pangkat' => 'c'
+      ]);
+        Pangkat::create([
+        'nama_pangkat' => 'Penata Tk. I',
+        'golongan_pangkat' => 'III/',
+        'ruang_pangkat' => 'd'
+      ]);
+        Pangkat::create([
+        'nama_pangkat' => 'Pembina',
+        'golongan_pangkat' => 'IV/',
+        'ruang_pangkat' => 'a'
+      ]);
+        Pangkat::create([
+        'nama_pangkat' => 'Pembina Tk. I',
+        'golongan_pangkat' => 'IV/',
+        'ruang_pangkat' => 'b'
+      ]);
+        Pangkat::create([
+        'nama_pangkat' => 'Pembina Utama Muda',
+        'golongan_pangkat' => 'IV/',
+        'ruang_pangkat' => 'c'
+      ]);
+          Pangkat::create([
+        'nama_pangkat' => 'Pembina Utama Madya',
+        'golongan_pangkat' => 'IV/',
+        'ruang_pangkat' => 'd'
+      ]);
+ 
+          Pangkat::create([
+        'nama_pangkat' => 'Pembina Utama',
+        'golongan_pangkat' => 'IV/',
+        'ruang_pangkat' => 'e'
+      ]);
+ 
+ 
  
       User::factory(3)->create();
       Post::factory(20)->create();
